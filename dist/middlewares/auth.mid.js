417eaa11ac2b69196e152f559b79346dc36ed72c
Mahmoud Rabea <88596640+mahmoudrabea28@users.noreply.github.com>
2025-03-03 00:15:07 +0200

    Add files via upload

--- dist/middlewares/auth.mid.js
+++ dist/middlewares/auth.mid.js
@@ -2,15 +2,31 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var jsonwebtoken_1 = require("jsonwebtoken");
 var http_status_1 = require("../constants/http_status");
+// export default (req: any, res: any, next: any) => {
+//     const token = req.headers.access_token as string;
+//     if(!token) return res.status(HTTP_UNAUTHORIZED).send();
+//     try {
+//         const decodedUser = verify(token, process.env.JWT_SECRET!);
+//         req.user = decodedUser;
+//     } catch (error) {
+//         res.status(HTTP_UNAUTHORIZED).send();
+//     }
+//     return next();
+// }
 exports.default = (function (req, res, next) {
     var token = req.headers.access_token;
-    if (!token)
+    console.log("Token received:", token); // Add this line
+    if (!token) {
+        console.log("Token missing"); //add this line
         return res.status(http_status_1.HTTP_UNAUTHORIZED).send();
+    }
     try {
         var decodedUser = (0, jsonwebtoken_1.verify)(token, process.env.JWT_SECRET);
         req.user = decodedUser;
+        console.log("Decoded user:", decodedUser); // Add this line
     }
     catch (error) {
+        console.log("Token verification failed:", error); // Add this line
         res.status(http_status_1.HTTP_UNAUTHORIZED).send();
     }
     return next();
