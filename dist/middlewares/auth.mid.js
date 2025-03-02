aa5926f0f38e5aa7035d194a4380f65bde704d88
Mahmoud Rabea <88596640+mahmoudrabea28@users.noreply.github.com>
2025-03-03 00:22:34 +0200

    Add files via upload

--- dist/middlewares/auth.mid.js
+++ dist/middlewares/auth.mid.js
@@ -1,7 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var jsonwebtoken_1 = require("jsonwebtoken");
-var http_status_1 = require("../constants/http_status");
 // export default (req: any, res: any, next: any) => {
 //     const token = req.headers.access_token as string;
 //     if(!token) return res.status(HTTP_UNAUTHORIZED).send();
@@ -16,18 +14,17 @@ var http_status_1 = require("../constants/http_status");
 exports.default = (function (req, res, next) {
     var token = req.headers.access_token;
     console.log("Token received:", token); // Add this line
-    if (!token) {
-        console.log("Token missing"); //add this line
-        return res.status(http_status_1.HTTP_UNAUTHORIZED).send();
-    }
-    try {
-        var decodedUser = (0, jsonwebtoken_1.verify)(token, process.env.JWT_SECRET);
-        req.user = decodedUser;
-        console.log("Decoded user:", decodedUser); // Add this line
-    }
-    catch (error) {
-        console.log("Token verification failed:", error); // Add this line
-        res.status(http_status_1.HTTP_UNAUTHORIZED).send();
-    }
+    // if (!token) {
+    //     console.log("Token missing"); //add this line
+    //     return res.status(HTTP_UNAUTHORIZED).send();
+    // }
+    // try {
+    //     const decodedUser = verify(token, process.env.JWT_SECRET!);
+    //     req.user = decodedUser;
+    //     console.log("Decoded user:", decodedUser); // Add this line
+    // } catch (error) {
+    //     console.log("Token verification failed:", error); // Add this line
+    //     res.status(HTTP_UNAUTHORIZED).send();
+    // }
     return next();
 });
